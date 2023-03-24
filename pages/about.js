b1528afccd3b24efab859140fd425a6110713402
Viktor <50581498+fvo69kiev@users.noreply.github.com>
2023-03-24 11:43:23 +0200

    Update about.js

--- pages/about.js
+++ pages/about.js
@@ -2,17 +2,13 @@ import Layout from '@/components/Layout'
 
 export default function AboutPage() {
   return (
-    <Layout title='About DevSpace'>
+    <Layout title='About DogBlog'>
       <h1 className='text-5xl border-b-4 pb-5 font-bold'>About</h1>
 
       <div className='bg-white shadow-md rounded-lg px-10 py-6 mt-6'>
-        <h3 className='text-2xl mb-5'>DevSpace Blog</h3>
-
-        <p className='mb-3'>This is a blog built with Next.js and Markdown</p>
-
-        <p>
-          <span className='font-bold'>Version 1.0.0</span>
-        </p>
+        <h3 className='text-2xl mb-5'>DogBlog</h3>
+        <p>Ми зібрали цікаві статті за темою грумінгу домашніх тварин.</p>
+        <p className='mb-3'>Цей блог побудований за новітніми технологіями Next.js та Markdown</p>
       </div>
     </Layout>
   )
